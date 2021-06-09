8a4d8b4f66dd1dd25f923ad2d0fd08d0a881f679
biubiuworld <77176093+biubiuworld@users.noreply.github.com>
2021-06-09 12:02:46 -0700

    result page

--- Summary/models.py
+++ Summary/models.py
@@ -21,25 +21,131 @@ class Constants(BaseConstants):
     name_in_url = 'Summary'
     players_per_group = None
     num_rounds = 1
+    endowment_game1 = 3
+    value_of_B_0p1_game1 = random.choices([3,0], weights = [1, 9])[0]
+    value_of_B_0p2_game1 = random.choices([3,0], weights = [2, 8])[0]
+    value_of_B_0p3_game1 = random.choices([3,0], weights = [3, 7])[0]
+    value_of_B_0p4_game1 = random.choices([3,0], weights = [4, 6])[0]
+    value_of_B_0p5_game1 = random.choices([3,0], weights = [5, 5])[0]
+    value_of_B_0p6_game1 = random.choices([3,0], weights = [6, 4])[0]
+    value_of_B_0p7_game1 = random.choices([3,0], weights = [7, 3])[0]
+    value_of_B_0p8_game1 = random.choices([3,0], weights = [8, 2])[0]
+    value_of_B_0p9_game1 = random.choices([3,0], weights = [9, 1])[0]
 
+    top_value_of_B_0p1_game2 = random.choices([3,1], weights = [1, 9])[0]
+    top_value_of_B_0p2_game2 = random.choices([3,1], weights = [2, 8])[0]
+    top_value_of_B_0p3_game2 = random.choices([3,1], weights = [3, 7])[0]
+    top_value_of_B_0p4_game2 = random.choices([3,1], weights = [4, 6])[0]
+    top_value_of_B_0p5_game2 = random.choices([3,1], weights = [5, 5])[0]
+    top_value_of_B_0p6_game2 = random.choices([3,1], weights = [6, 4])[0]
+    top_value_of_B_0p7_game2 = random.choices([3,1], weights = [7, 3])[0]
+    top_value_of_B_0p8_game2 = random.choices([3,1], weights = [8, 2])[0]
+    top_value_of_B_0p9_game2 = random.choices([3,1], weights = [9, 1])[0]
+
+    bottom_value_of_B_0p1_game2 = random.choices([1,0], weights = [1, 9])[0]
+    bottom_value_of_B_0p2_game2 = random.choices([1,0], weights = [2, 8])[0]
+    bottom_value_of_B_0p3_game2 = random.choices([1,0], weights = [3, 7])[0]
+    bottom_value_of_B_0p4_game2 = random.choices([1,0], weights = [4, 6])[0]
+    bottom_value_of_B_0p5_game2 = random.choices([1,0], weights = [5, 5])[0]
+    bottom_value_of_B_0p6_game2 = random.choices([1,0], weights = [6, 4])[0]
+    bottom_value_of_B_0p7_game2 = random.choices([1,0], weights = [7, 3])[0]
+    bottom_value_of_B_0p8_game2 = random.choices([1,0], weights = [8, 2])[0]
+    bottom_value_of_B_0p9_game2 = random.choices([1,0], weights = [9, 1])[0]
 
 class Subsession(BaseSubsession):
     pass
 
 
 class Group(BaseGroup):
-    pass
+    def get_payoff(self):
+        # second investment game payoff
+        for p in self.get_players():
+            p.participant.vars['endowment_game1'] = Constants.endowment_game1
+            p.participant.vars['value_of_B_0p1_game1'] = Constants.value_of_B_0p1_game1
+            p.participant.vars['value_of_B_0p2_game1'] = Constants.value_of_B_0p2_game1
+            p.participant.vars['value_of_B_0p3_game1'] = Constants.value_of_B_0p3_game1
+            p.participant.vars['value_of_B_0p4_game1'] = Constants.value_of_B_0p4_game1
+            p.participant.vars['value_of_B_0p5_game1'] = Constants.value_of_B_0p5_game1
+            p.participant.vars['value_of_B_0p6_game1'] = Constants.value_of_B_0p6_game1
+            p.participant.vars['value_of_B_0p7_game1'] = Constants.value_of_B_0p7_game1
+            p.participant.vars['value_of_B_0p8_game1'] = Constants.value_of_B_0p8_game1
+            p.participant.vars['value_of_B_0p9_game1'] = Constants.value_of_B_0p9_game1
+        for p in self.get_players():
+            p.participant.vars['payoff_game1_p10'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider1']*0.01*1 + (100-p.participant.vars['game1_slider1'])*0.01*p.participant.vars['value_of_B_0p1_game1']), 2)
+            p.participant.vars['payoff_game1_p20'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider2']*0.01*1 + (100-p.participant.vars['game1_slider2'])*0.01*p.participant.vars['value_of_B_0p2_game1']), 2)
+            p.participant.vars['payoff_game1_p30'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider3']*0.01*1 + (100-p.participant.vars['game1_slider3'])*0.01*p.participant.vars['value_of_B_0p3_game1']), 2)
+            p.participant.vars['payoff_game1_p40'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider4']*0.01*1 + (100-p.participant.vars['game1_slider4'])*0.01*p.participant.vars['value_of_B_0p4_game1']), 2)
+            p.participant.vars['payoff_game1_p50'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider5']*0.01*1 + (100-p.participant.vars['game1_slider5'])*0.01*p.participant.vars['value_of_B_0p5_game1']), 2)
+            p.participant.vars['payoff_game1_p60'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider6']*0.01*1 + (100-p.participant.vars['game1_slider6'])*0.01*p.participant.vars['value_of_B_0p6_game1']), 2)
+            p.participant.vars['payoff_game1_p70'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider7']*0.01*1 + (100-p.participant.vars['game1_slider7'])*0.01*p.participant.vars['value_of_B_0p7_game1']), 2)
+            p.participant.vars['payoff_game1_p80'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider8']*0.01*1 + (100-p.participant.vars['game1_slider8'])*0.01*p.participant.vars['value_of_B_0p8_game1']), 2)
+            p.participant.vars['payoff_game1_p90'] = round(p.participant.vars['endowment_game1']*(p.participant.vars['game1_slider9']*0.01*1 + (100-p.participant.vars['game1_slider9'])*0.01*p.participant.vars['value_of_B_0p9_game1']), 2)
+
+        # second investment game payoff
+        for p in self.get_players():
+            p.participant.vars['endowment_game2'] = p.participant.vars["num_of_correct_answers_part2"]
+        for p in self.get_players():
+            if p.participant.vars['ranking_subgroup']>=50:
+                p.participant.vars['value_of_B_0p1_game2'] = Constants.top_value_of_B_0p1_game2
+                p.participant.vars['value_of_B_0p2_game2'] = Constants.top_value_of_B_0p2_game2
+                p.participant.vars['value_of_B_0p3_game2'] = Constants.top_value_of_B_0p3_game2
+                p.participant.vars['value_of_B_0p4_game2'] = Constants.top_value_of_B_0p4_game2
+                p.participant.vars['value_of_B_0p5_game2'] = Constants.top_value_of_B_0p5_game2
+                p.participant.vars['value_of_B_0p6_game2'] = Constants.top_value_of_B_0p6_game2
+                p.participant.vars['value_of_B_0p7_game2'] = Constants.top_value_of_B_0p7_game2
+                p.participant.vars['value_of_B_0p8_game2'] = Constants.top_value_of_B_0p8_game2
+                p.participant.vars['value_of_B_0p9_game2'] = Constants.top_value_of_B_0p9_game2
+            else:
+                p.participant.vars['value_of_B_0p1_game2'] = Constants.bottom_value_of_B_0p1_game2
+                p.participant.vars['value_of_B_0p2_game2'] = Constants.bottom_value_of_B_0p2_game2
+                p.participant.vars['value_of_B_0p3_game2'] = Constants.bottom_value_of_B_0p3_game2
+                p.participant.vars['value_of_B_0p4_game2'] = Constants.bottom_value_of_B_0p4_game2
+                p.participant.vars['value_of_B_0p5_game2'] = Constants.bottom_value_of_B_0p5_game2
+                p.participant.vars['value_of_B_0p6_game2'] = Constants.bottom_value_of_B_0p6_game2
+                p.participant.vars['value_of_B_0p7_game2'] = Constants.bottom_value_of_B_0p7_game2
+                p.participant.vars['value_of_B_0p8_game2'] = Constants.bottom_value_of_B_0p8_game2
+                p.participant.vars['value_of_B_0p9_game2'] = Constants.bottom_value_of_B_0p9_game2
+
+        for p in self.get_players():
+            p.participant.vars['payoff_game2_p10'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider1'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider1']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p1_game2']), 2)
+            p.participant.vars['payoff_game2_p20'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider2'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider2']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p2_game2']), 2)
+            p.participant.vars['payoff_game2_p30'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider3'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider3']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p3_game2']), 2)
+            p.participant.vars['payoff_game2_p40'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider4'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider4']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p4_game2']), 2)
+            p.participant.vars['payoff_game2_p50'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider5'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider5']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p5_game2']), 2)
+            p.participant.vars['payoff_game2_p60'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider6'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider6']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p6_game2']), 2)
+            p.participant.vars['payoff_game2_p70'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider7'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider7']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p7_game2']), 2)
+            p.participant.vars['payoff_game2_p80'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider8'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider8']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p8_game2']), 2)
+            p.participant.vars['payoff_game2_p90'] = round(p.participant.vars['endowment_game2'] * (
+                        p.participant.vars['game2_slider9'] * 0.01 * 1 + (
+                            100 - p.participant.vars['game2_slider9']) * 0.01 * p.participant.vars[
+                            'value_of_B_0p9_game2']), 2)
 
 
 class Player(BasePlayer):
     pass
-    # value_of_B_0p1_game1 = random.choices([3,1], weights = [1, 9])[0]
-    # value_of_B_0p2_game1 = random.choices([3,1], weights = [2, 8])[0]
-    # value_of_B_0p3_game1 = random.choices([3,1], weights = [3, 7])[0]
-    # value_of_B_0p4_game1 = random.choices([3,1], weights = [4, 6])[0]
-    # value_of_B_0p5_game1 = random.choices([3,1], weights = [5, 5])[0]
-    # value_of_B_0p6_game1 = random.choices([3,1], weights = [6, 4])[0]
-    # value_of_B_0p7_game1 = random.choices([3,1], weights = [7, 3])[0]
-    # value_of_B_0p8_game1 = random.choices([3,1], weights = [8, 2])[0]
-    # value_of_B_0p9_game1 = random.choices([3,1], weights = [9, 1])[0]
+
 
--- Summary/pages.py
+++ Summary/pages.py
@@ -52,11 +52,96 @@ class Summary(Page):
 
 
 class ResultsWaitPage(WaitPage):
-    pass
+    after_all_players_arrive = "get_payoff"
 
 
 class Results(Page):
-    pass
+    def vars_for_template(self):
+        return{
+            'num_of_correct_answers_part1': self.participant.vars["num_of_correct_answers_part1"],
+            'num_of_correct_answers_part2': self.participant.vars["num_of_correct_answers_part2"],
+            'self_evaluation1': self.participant.vars['self_evaluation1'],
+            'self_evaluation2': self.participant.vars['self_evaluation2'],
+            'ranking': self.participant.vars['ranking'],
+            'ranking_subgroup': self.participant.vars['ranking_subgroup'],
+            'game1_accountA_p10': self.participant.vars['game1_slider1'],
+            'game1_accountB_p10': 100-self.participant.vars['game1_slider1'],
+            'game1_accountA_p20': self.participant.vars['game1_slider2'],
+            'game1_accountB_p20': 100 - self.participant.vars['game1_slider2'],
+            'game1_accountA_p30': self.participant.vars['game1_slider3'],
+            'game1_accountB_p30': 100 - self.participant.vars['game1_slider3'],
+            'game1_accountA_p40': self.participant.vars['game1_slider4'],
+            'game1_accountB_p40': 100 - self.participant.vars['game1_slider4'],
+            'game1_accountA_p50': self.participant.vars['game1_slider5'],
+            'game1_accountB_p50': 100 - self.participant.vars['game1_slider5'],
+            'game1_accountA_p60': self.participant.vars['game1_slider6'],
+            'game1_accountB_p60': 100 - self.participant.vars['game1_slider6'],
+            'game1_accountA_p70': self.participant.vars['game1_slider7'],
+            'game1_accountB_p70': 100 - self.participant.vars['game1_slider7'],
+            'game1_accountA_p80': self.participant.vars['game1_slider8'],
+            'game1_accountB_p80': 100 - self.participant.vars['game1_slider8'],
+            'game1_accountA_p90': self.participant.vars['game1_slider9'],
+            'game1_accountB_p90': 100 - self.participant.vars['game1_slider9'],
+            'game2_accountA_p10': self.participant.vars['game2_slider1'],
+            'game2_accountB_p10': 100 - self.participant.vars['game2_slider1'],
+            'game2_accountA_p20': self.participant.vars['game2_slider2'],
+            'game2_accountB_p20': 100 - self.participant.vars['game2_slider2'],
+            'game2_accountA_p30': self.participant.vars['game2_slider3'],
+            'game2_accountB_p30': 100 - self.participant.vars['game2_slider3'],
+            'game2_accountA_p40': self.participant.vars['game2_slider4'],
+            'game2_accountB_p40': 100 - self.participant.vars['game2_slider4'],
+            'game2_accountA_p50': self.participant.vars['game2_slider5'],
+            'game2_accountB_p50': 100 - self.participant.vars['game2_slider5'],
+            'game2_accountA_p60': self.participant.vars['game2_slider6'],
+            'game2_accountB_p60': 100 - self.participant.vars['game2_slider6'],
+            'game2_accountA_p70': self.participant.vars['game2_slider7'],
+            'game2_accountB_p70': 100 - self.participant.vars['game2_slider7'],
+            'game2_accountA_p80': self.participant.vars['game2_slider8'],
+            'game2_accountB_p80': 100 - self.participant.vars['game2_slider8'],
+            'game2_accountA_p90': self.participant.vars['game2_slider9'],
+            'game2_accountB_p90': 100 - self.participant.vars['game2_slider9'],
+            'endowment_game1': self.participant.vars['endowment_game1'],
+            'value_of_B_0p1_game1': self.participant.vars['value_of_B_0p1_game1'],
+            'value_of_B_0p2_game1': self.participant.vars['value_of_B_0p2_game1'],
+            'value_of_B_0p3_game1': self.participant.vars['value_of_B_0p3_game1'],
+            'value_of_B_0p4_game1': self.participant.vars['value_of_B_0p4_game1'],
+            'value_of_B_0p5_game1': self.participant.vars['value_of_B_0p5_game1'],
+            'value_of_B_0p6_game1': self.participant.vars['value_of_B_0p6_game1'],
+            'value_of_B_0p7_game1': self.participant.vars['value_of_B_0p7_game1'],
+            'value_of_B_0p8_game1': self.participant.vars['value_of_B_0p8_game1'],
+            'value_of_B_0p9_game1': self.participant.vars['value_of_B_0p9_game1'],
+            'payoff_game1_p10': self.participant.vars['payoff_game1_p10'],
+            'payoff_game1_p20': self.participant.vars['payoff_game1_p20'],
+            'payoff_game1_p30': self.participant.vars['payoff_game1_p30'],
+            'payoff_game1_p40': self.participant.vars['payoff_game1_p40'],
+            'payoff_game1_p50': self.participant.vars['payoff_game1_p50'],
+            'payoff_game1_p60': self.participant.vars['payoff_game1_p60'],
+            'payoff_game1_p70': self.participant.vars['payoff_game1_p70'],
+            'payoff_game1_p80': self.participant.vars['payoff_game1_p80'],
+            'payoff_game1_p90': self.participant.vars['payoff_game1_p90'],
+            'endowment_game2': self.participant.vars['endowment_game2'],
+            'value_of_B_0p1_game2': self.participant.vars['value_of_B_0p1_game2'],
+            'value_of_B_0p2_game2': self.participant.vars['value_of_B_0p2_game2'],
+            'value_of_B_0p3_game2': self.participant.vars['value_of_B_0p3_game2'],
+            'value_of_B_0p4_game2': self.participant.vars['value_of_B_0p4_game2'],
+            'value_of_B_0p5_game2': self.participant.vars['value_of_B_0p5_game2'],
+            'value_of_B_0p6_game2': self.participant.vars['value_of_B_0p6_game2'],
+            'value_of_B_0p7_game2': self.participant.vars['value_of_B_0p7_game2'],
+            'value_of_B_0p8_game2': self.participant.vars['value_of_B_0p8_game2'],
+            'value_of_B_0p9_game2': self.participant.vars['value_of_B_0p9_game2'],
+            'payoff_game2_p10': self.participant.vars['payoff_game2_p10'],
+            'payoff_game2_p20': self.participant.vars['payoff_game2_p20'],
+            'payoff_game2_p30': self.participant.vars['payoff_game2_p30'],
+            'payoff_game2_p40': self.participant.vars['payoff_game2_p40'],
+            'payoff_game2_p50': self.participant.vars['payoff_game2_p50'],
+            'payoff_game2_p60': self.participant.vars['payoff_game2_p60'],
+            'payoff_game2_p70': self.participant.vars['payoff_game2_p70'],
+            'payoff_game2_p80': self.participant.vars['payoff_game2_p80'],
+            'payoff_game2_p90': self.participant.vars['payoff_game2_p90'],
+
+        }
+
+
 
 
 page_sequence = [Summary, ResultsWaitPage, Results]
